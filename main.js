d230e3fd23b5b3699dce5a2b81798c63e7cd35a3
Pablo Jurado <pjurado85@gmail.com>
2017-04-01 10:18:18 -0500

    finish task 2

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 
-document.querySelector("#thanks button").addEventListener('click',function(){
+document.querySelector('#thanks button').addEventListener('click', function () {
   // TASK #1
+  alert('yowch! don\'t click me so hard!')
 })
 
-document.querySelector("#double button").addEventListener('click',function(){
+document.querySelector('#double button').addEventListener('click', function () {
   // TASK #2
+  var pElement = document.querySelector('#compoundInvestment')
+  pElement.innerHTML *= 2
 })
 
-
-document.querySelector("#color-circle button").addEventListener('click',function(){
+document.querySelector('#color-circle button').addEventListener('click', function () {
   // TASK #3
 })
 
-document.querySelector("#blow-up button").addEventListener('click',function(){
+document.querySelector('#blow-up button').addEventListener('click', function () {
   // TASK #4}
 })
 
-document.querySelector("#remove button").addEventListener('click',function(){
+document.querySelector('#remove button').addEventListener('click', function () {
   // TASK #5
 })
 
-
-document.querySelector("#reverse-squares button").addEventListener('click',function(){
+document.querySelector('#reverse-squares button').addEventListener('click', function () {
   // TASK #6
 })
 
-document.querySelector("#pig-latin button").addEventListener('click',function(){
+document.querySelector('#pig-latin button').addEventListener('click', function () {
   // TASK #7
 })
 
-document.querySelector("#cycle-image button").addEventListener('click',function(){
+document.querySelector('#cycle-image button').addEventListener('click', function () {
   // TASK #8
 })
